686c107b4a30ae3209d5156ac40cb539356100f7
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-22 17:33:27 -0300

    form busca - botoes -chips - campos form - capos data

---
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-form-busca',
+  templateUrl: './form-busca.component.html',
+  styleUrls: ['./form-busca.component.scss']
+})
+export class FormBuscaComponent {
+
+  @Input() variant: 'primary' | 'secondary' = 'primary'
+
+  public openDialog() {
+
+  }
+
+}
